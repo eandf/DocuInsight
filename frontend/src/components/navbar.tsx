78cf545d786d78237b538ead417d32d649c308fb
Dylan Eck <akdylaneck@gmail.com>
2025-01-23 07:31:36 -0600

    add link to home to navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,30 +1,35 @@
 import { auth, signOut } from "@/auth";
 import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Navbar() {
   const session = await auth();
 
   return (
-    <nav className="flex px-4 py-2 items-center justify-between max-w-screen-xl mx-auto h-16 border-b">
-      <span className="text-2xl font-medium">Docuinsight</span>
-      <ul className="flex gap-2">
-        <li>
-          {session ? (
-            <form
-              action={async () => {
-                "use server";
-                await signOut();
-              }}
-            >
-              <Button type="submit">Sign out</Button>
-            </form>
-          ) : (
-            <a href="/auth/sign-in" className={buttonVariants()}>
-              Sign In
-            </a>
-          )}
-        </li>
-      </ul>
-    </nav>
+    <div className="w-full border-b">
+      <nav className="flex px-4 py-2 items-center justify-between max-w-screen-xl mx-auto h-16">
+        <Link className="text-2xl font-medium" href="/">
+          Docuinsight
+        </Link>
+        <ul className="flex gap-2">
+          <li>
+            {session ? (
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <Button type="submit">Sign out</Button>
+              </form>
+            ) : (
+              <a href="/auth/sign-in" className={buttonVariants()}>
+                Sign In
+              </a>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
